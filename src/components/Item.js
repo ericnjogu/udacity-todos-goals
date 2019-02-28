de5b636ec8b74c1a972db7f0438acf1e7862c6a5
Eric Njogu <kunadawa@gmail.com>
2019-02-28 10:19:15 +0300

    fix: action handlers have to be called via dispatch()
    Goals toggle and remove working okay but TODOs is buggy

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,14 +1,17 @@
 import React from 'react'
+import {connect} from 'react-redux'
 
-export default function Item(props) {
+function Item(props) {
     return (
         <li>
-            <span onClick={() => props.toggle(props.item.id)}
+            <span onClick={() => props.dispatch(props.toggle(props.item.id))}
                   style={{textDecoration : props.item.complete ? 'line-through' : 'none'}}>
             {props.item.name}
             </span>
             &nbsp;
-            <button onClick={() => props.remove(props.item)}>xXx</button>
+            <button onClick={() => props.dispatch(props.remove(props.item))}>xXx</button>
         </li>
     )
 }
+
+export default connect()(Item)
